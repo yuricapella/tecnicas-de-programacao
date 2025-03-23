c5ed02cb1c901df7ebd5190ef7750b4a10517f48
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-03-23 11:52:28 -0300

    refatora pasta de exercicios com data para melhor organizaçÃão

---
+++ src/lista_de_exercicios_lms/data_e_java_time/exercicio_07/CalculoData.java
@@ -0,0 +1,16 @@
+package lista_de_exercicios_lms.data_e_java_time.exercicio_07;
+
+import java.time.LocalDate;
+
+public class CalculoData {
+    public static void main(String[] args) {
+        LocalDate data = LocalDate.now();
+        int dias = 15;
+        LocalDate dataNofuturo = avancaDias(data,dias);
+        System.out.println(dataNofuturo);
+    }
+
+    public static LocalDate avancaDias(LocalDate data, int dias) {
+        return data.plusDays(dias);
+    }
+}
